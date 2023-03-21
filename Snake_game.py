86c36beefff64eacce534fc7614f190ec6fca45c
lorenzorogn <lorenzo.rognini@yahoo.com>
2023-03-21 12:11:48 +0100

    21/03

--- Snake_game.py
+++ Snake_game.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from SnakeFood import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -19,10 +21,12 @@ screen.onkey(snake.left, "Left")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1 )
- 
+    time.sleep(0.1)
     snake.move()
 
+    # detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 
